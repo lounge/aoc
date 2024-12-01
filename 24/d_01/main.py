0255c78d164c13a50bc8cb12947b45b501c0b35a
lounge <fredlunden@gmail.com>
2024-12-01 20:52:51 +0100

    refac

--- 24/d_01/main.py
+++ 24/d_01/main.py
@@ -1,28 +1,22 @@
 import os, sys, re
 
-l1 = []
-l2 = []
-def q1():
-    score = 0
-    for i in range(len(l1)):
-        score += abs(l1[i]-l2[i])
-    return score
+def q1(zip):
+    return sum([abs(x - y) for x, y in zip])
 
 
-def q2():
-    score = 0
-    for i in range(len(l1)):
-       score += l1[i] * l2.count(l1[i])
-    return score
-
+def q2(l1, l2):
+    return sum(x * l2.count(x) for x in l1)
 
 with open(os.path.join(sys.path[0], "input_01.txt"), "r") as file:  
-    while line := file.readline().rstrip():
-        d = re.findall(r"\d+", line.rstrip())
+    l1, l2 = [], []
+
+    while line := file.readline():
+        d = re.findall(r"\d+", line.strip())
         l1.append(int(d[0]))
         l2.append(int(d[1]))
 
     l1.sort()
     l2.sort()
-    print(q1())
-    print(q2())
+
+    print(q1(zip(l1, l2)))
+    print(q2(l1, l2))
