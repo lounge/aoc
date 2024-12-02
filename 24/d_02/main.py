b7bf898599e6bf55acc26467614a29bf49e5e212
lounge <fredlunden@gmail.com>
2024-12-02 23:03:06 +0100

    d_02

---
+++ 24/d_02/main.py
@@ -0,0 +1,46 @@
+import os, sys, re
+
+score1 = 0;
+
+def check_diffs(diffs):
+    return all([-3 <= d < 0 for d in diffs]) or all([0 < d <= 3 for d in diffs])
+
+def q1(diff):
+    if check_diffs(diff):
+        return 1
+    
+    return 0
+
+def q2(diff):
+    if not check_diffs(diff):
+        for i in range(len(report)):
+            fixed = diff.copy()
+            if i == 0:   
+                fixed.pop(0)
+            elif i == len(report)-1: 
+                fixed.pop(-1)
+            else:                    
+                fixed[i-1] += fixed.pop(i)
+
+            if check_diffs(fixed):
+                return 1
+    
+    return 0
+
+
+with open(os.path.join(sys.path[0], "input_01.txt"), "r") as file:  
+    score1, score2 = 0, 0
+
+    while line := file.readline():
+        d = re.findall(r"\d+", line.strip())
+
+        report = list(map(int, d))
+        diffs = [a-b for a, b in zip(report[0::], report[1::])]
+
+        score1 += q1(diffs)
+        score2 += q2(diffs)
+
+    print(score1)
+    print(score1 + score2)
+
+
